c7cc10f15262c867d9b58b99724e8e1a903fa328
Evan Kaminsky <evanskaminsky@gmail.com>
2017-11-05 18:22:10 -0600

    initial commit

---
+++ scripts.js
@@ -0,0 +1,63 @@
+function stats() {
+    console.log("Reads: "     + debug.metrics(true).eth.db.chaindata.user.readscount.Overall);
+    console.log("Writes: "    + debug.metrics(true).eth.db.chaindata.user.writescount.Overall);
+    console.log("Pending: "   + eth.pendingTransactions.length);
+    console.log("Pending-2: " + eth.getBlock("pending").transactions.length);
+    console.log("Peers: "     + admin.peers.length);
+    console.log("Enode: "     + admin.nodeInfo.enode);
+    console.log("Block #: "   + eth.blockNumber);
+}
+
+function pendingTransactions() {
+    return eth.getBlock("pending").transactions.length > 0;
+}
+
+function lookForTransactions() {
+    if (pendingTransactions()) {
+        console.log("Already have pending transactions! Terminating.");
+        return;
+    }
+
+    console.log("Looking for transactions...");
+    miner.start(1);
+
+    while (!pendingTransactions() && !kill) {}
+
+    console.log("Found pending transactions! Terminating.");
+    miner.stop();
+}
+
+function mineWhilePending() {
+    console.log("Starting to mine...");
+    miner.start(1);
+
+    while (pendingTransactions() && !kill) {}
+
+    console.log("No more transactions! Terminating.");
+    miner.stop();
+}
+
+function sendTransactions(quantity) {
+    if (quantity < 1 || quantity > 100000) {
+        console.log("Can't send " + quantity + " transactions. Terminating.");
+        return;
+    }
+
+    eth.defaultAccount = eth.accounts[0];
+    personal.unlockAccount(eth.accounts[0], "1234");
+
+    console.log("Sending " + quantity + " transactions!");
+    for (i = 0; i < quantity; i++) {
+        eth.sendTransaction({from: eth.accounts[0], to: eth.accounts[1], value: 0x1})
+    }
+
+    console.log("Finished sending the transactions. Terminating.");
+}
+
+function checkBlocks(index) {
+    for (i = 0 ; i < index; i++) {
+        if (eth.getBlock(i).transactions.length > 0) {
+            console.log(i);
+        }
+    }
+}
